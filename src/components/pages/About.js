9a45fefae6e60c04682f94112ce217b299225934
Bart Frackiewicz <decomplosed@gmail.com>
2019-11-24 21:25:20 +0100

    set initial strucutre for an app and added Router so it will lead to the proper pages

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react'
+
+const About = () => {
+  return (
+    <Fragment>
+      <h1>About this App</h1>
+      <p>A simple app to see the list of all products and update them</p>
+    </Fragment>
+  )
+}
+
+export default About
