9358e999c1ac897e28341b04fda4d352b181691b
Bart Frackiewicz <decomplosed@gmail.com>
2019-11-27 23:08:34 +0100

    fetching the data about a single product

--- src/components/products/ProductDetails.js
+++ src/components/products/ProductDetails.js
@@ -1,12 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState, Fragment } from 'react'
 import { Link } from 'react-router-dom'
 
+const ProductDetails = ({ match }) => {
+  useEffect(() => {
+    fetchProduct()
+    // eslint-disable-next-line
+  }, [])
 
-const ProductDetails = ({ currentProduct }) => {
-  return (
-    <div>
+  const [product, setProduct] = useState({})
+
+  const fetchProduct = async () => {
+    const fetchProduct = await fetch(`http://localhost:3000/products?number=${match.params.number}`)
+
+    const product = await fetchProduct.json()
+
+    console.log(product);
+
+    setProduct(product)
+  }
+
+  return <Fragment>
+    <Link to='/' className="btn btn-light">Back To List</Link>
+    <div className="card">
+      <div className="all-center">
+        <h1>{}</h1>
+      </div>
     </div>
-  )
+  </Fragment>
 }
 
 export default ProductDetails
