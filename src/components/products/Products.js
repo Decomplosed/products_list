e6931709b753f8a738051ccefaf67499a892ee1d
Bart Frackiewicz <decomplosed@gmail.com>
2019-11-27 23:08:23 +0100

    fetch the products data from api

--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -1,46 +1,27 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import ProductsContext from '../../context/products/productsContext'
 import ProductItem from './ProductItem'
 
+
 const Products = () => {
-  //   const productsContext = useContext(ProductsContext)
 
-  //   const products = productsContext
+  // const productsContext = useContext(ProductsContext)
 
-  const products = [
-    {
-      "name": "singing coach unlimited",
-      "number": "b0006se5bq",
-      "description": "singing coach unlimited - electronic learning products (win me nt 2000 xp)",
-      "images": [
-        {
-          "url": "http://lorempixel.com/200/200/technics/",
-          "name": "singing coach"
-        },
-        {
-          "url": "http://lorempixel.com/200/200/abstract/",
-          "name": "front side"
-        }
-      ]
-    },
-    {
-      "name": "adobe after effects professional 6.5 upgrade from standard to professional",
-      "number": "b00021xhzw",
-      "description": "upgrade only; installation of after effects standard new disk caching tools speed up your interactive work save any combination of animation parameters as presets",
-      "images": []
-    },
-    {
-      "name": "domino designer/developer v5.0",
-      "number": "b00021xhzy",
-      "description": "reference domino designer/developer r5 doc pack includes the following titles: application development with domino designer (intermediate-advanced) 536 pages it explains building applications creating databases using forms fields views folders navi",
-      "images": [
-        {
-          "url": "http://lorempixel.com/200/200/people/",
-          "name": "cover"
-        }
-      ]
-    }
-  ]
+  // const { products } = productsContext
+
+  useEffect(() => {
+    fetchProducts()
+  }, [])
+
+  const [products, setProducts] = useState([])
+
+  const fetchProducts = async () => {
+    const data = await fetch('http://localhost:3000/products')
+
+    const products = await data.json()
+
+    setProducts(products)
+  }
 
   return (
     <ul style={productStyle}>
