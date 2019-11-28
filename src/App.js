1ef79e4ff2b568cfe93f00a531d8c8a1e61022b6
Bart Frackiewicz <decomplosed@gmail.com>
2019-11-28 22:41:17 +0100

    closed the App inside context provider

--- src/App.js
+++ src/App.js
@@ -1,29 +1,34 @@
-import React from 'react';
+import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import ProductsList from './components/pages/ProductsList'
 import About from './components/pages/About'
 import NotFound from './components/pages/NotFound'
-import ProductDetails from './components/products/ProductDetails';
+import ProductDetails from './components/products/ProductDetails'
+import ProductEdit from './components/products/ProductEdit'
+import ProductsProvider from './context/products/ProductsProvider'
 
-import './App.css';
+import './App.css'
 
 const App = () => {
   return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <div className="container">
-          <Switch>
-            <Route exact path='/' component={ProductsList} />
-            <Route exact path='/about' component={About} />
-            <Route exact path='/product/:number' component={ProductDetails} />
-            <Route component={NotFound} />
-          </Switch>
+    <ProductsProvider>
+      <Router>
+        <div className='App'>
+          <Navbar />
+          <div className='container'>
+            <Switch>
+              <Route exact path='/' component={ProductsList} />
+              <Route exact path='/about' component={About} />
+              <Route exact path='/product/:number' component={ProductDetails} />
+              <Route exact path='/product/:number/edit' component={ProductEdit} />
+              <Route component={NotFound} />
+            </Switch>
+          </div>
         </div>
-      </div>
-    </Router>
-  );
+      </Router>
+    </ProductsProvider>
+  )
 }
 
-export default App;
+export default App
