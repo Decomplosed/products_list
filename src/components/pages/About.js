daafd90f27b74dc62b5daeb079442733072a6c9b
Bart Frackiewicz <decomplosed@gmail.com>
2019-11-28 22:39:38 +0100

    standard fixes

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,10 +2,10 @@ import React, { Fragment } from 'react'
 
 const About = () => {
   return (
-    <Fragment>
+    <>
       <h1>About this App</h1>
       <p>This is a simple app written in React to see the list of all products and do some actions with every element inside Products List</p>
-    </Fragment>
+    </>
   )
 }
 
