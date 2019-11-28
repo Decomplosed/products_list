c6a91eccdbecee126a4c6fc0d959973906007b3e
Bart Frackiewicz <decomplosed@gmail.com>
2019-11-28 22:44:15 +0100

    added context to the CProductsList component

--- src/components/pages/ProductsList.js
+++ src/components/pages/ProductsList.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import axios from 'axios'
 import Products from '../products/Products'
+import productsContext from '../../context/products/productsContext'
+import { GET_PRODUCTS } from '../../context/types'
+
+const getProducts = async () => {
+  const res = await axios.get('http://localhost:3000/products')
+
+  return res.data
+}
 
 const ProductsList = () => {
+  const { dispatch } = useContext(productsContext)
+
+  useEffect(() => {
+    getProducts().then(products => {
+      dispatch({
+        type: GET_PRODUCTS,
+        payload: products
+      })
+    })
+  }, [dispatch])
+
   return (
     <Products />
   )
