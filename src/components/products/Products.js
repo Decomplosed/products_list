9e1cfa8c500ce3d753754a36891b38c62f4cdfa9
Bart Frackiewicz <decomplosed@gmail.com>
2019-11-28 22:43:13 +0100

    added context to the products component

--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -1,27 +1,11 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext } from 'react'
 import ProductsContext from '../../context/products/productsContext'
 import ProductItem from './ProductItem'
 
-
 const Products = () => {
+  const productsContext = useContext(ProductsContext)
 
-  // const productsContext = useContext(ProductsContext)
-
-  // const { products } = productsContext
-
-  useEffect(() => {
-    fetchProducts()
-  }, [])
-
-  const [products, setProducts] = useState([])
-
-  const fetchProducts = async () => {
-    const data = await fetch('http://localhost:3000/products')
-
-    const products = await data.json()
-
-    setProducts(products)
-  }
+  const { products } = productsContext
 
   return (
     <ul style={productStyle}>
